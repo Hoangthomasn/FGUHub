8b9affa48ac775731915c56edee10dc66cf58355
Aceunit <hoang.thomas.n@gmail.com>
2026-03-15 17:23:10 -0500

    Initial commit

---
+++ CampaignRegistry.lua
@@ -0,0 +1,219 @@
+{
+	["autoload"] = {
+		["disable"] = true,
+	},
+	["OptEXTRAPLANAR_VOLUME"] = "off",
+	["OptNPCF_NONID"] = "type",
+	["OptPSIN"] = "on",
+	["OptHRDD"] = "raw",
+	["OptRSHE"] = "off",
+	["OptHRNH"] = "off",
+	["OptEXTRAPLANAR_COUNT"] = "off",
+	["sidebarexpand"] = {
+		[1] = "campaign",
+		[2] = "world",
+		[3] = "player",
+		[4] = "tool",
+		[5] = "create",
+		[6] = "library",
+	},
+	["OptTOAST"] = "off",
+	["OptSHPC"] = "detailed",
+	["OptHRFC"] = "both",
+	["OptCLSH"] = "active",
+	["OptANIMALSHAPE_RULES"] = "Default",
+	["OptTGME"] = "on",
+	["OptHRRA"] = "0.25",
+	["OptNPCENGAGE"] = "off",
+	["OptBARC"] = "",
+	["OptEMPTY_PC_NPC_LOOT_PARCEL"] = "on",
+	["OptSET_GROUPS_COMMON_INIT"] = "off",
+	["OptTNPCH"] = "dot",
+	["OptDESKTOPCTSIZE"] = "6",
+	["OptMANUALROLL"] = "off",
+	["OptRING"] = "on",
+	["OptHRIS"] = "",
+	["OptHRIR"] = "off",
+	["OptDISPLAY_GROUP"] = "off",
+	["OptCURR"] = "on",
+	["OptHREN"] = "",
+	["OptALWAYS_SHOW_OWNED_TOKENS"] = "on",
+	["OptDDCL-custom"] = "",
+	["desktopdicedisable"] = {
+	},
+	["OptTESZ"] = "",
+	["OptENABLEACTIONLOGGING"] = "enabled",
+	["OptTFAC"] = "off",
+	["OptRANDCRUEL"] = "on",
+	["OptNPCF_FCHANCE"] = "90%",
+	["OptCTSD"] = "on",
+	["DD2024Rules"] = "2014",
+	["sidebarvisibility"] = 0,
+	["OptRANDTGT"] = "logical_chance",
+	["OptTSOS"] = "off",
+	["OptWILDSHAPE_RULES"] = "Default",
+	["OptRANDVERB"] = "verbose",
+	["OptRANDPLYR"] = "muted",
+	["OptWSAV"] = "on",
+	["OptDISABLE_COMBATLISTMANAGER"] = "on",
+	["windowstate"] = {
+		["initind_select"] = {
+			["viewtype"] = "grid",
+			["viewzoom"] = 1,
+		},
+		["decal_select"] = {
+			["viewtype"] = "grid",
+			["viewzoom"] = 1,
+		},
+		["reference_manual"] = {
+			["reference.refmanualindex@Monster Loot - Lost Mine of Phandelver"] = {
+				["sPageRecord"] = "reference.refmanualindex.chapters.id-00001.subchapters.id-00001.refpages.id-00001@Monster Loot - Lost Mine of Phandelver",
+			},
+			["reference.refmanualindex@DD Dungeon Masters Guide"] = {
+				["sPageRecord"] = "reference.refmanualindex.chapters.chapter_0.subchapters.subchapter_10.refpages.refpage_03@DD Dungeon Masters Guide",
+			},
+			["reference.refmanualindex@Storm Lord's Wrath"] = {
+				["sPageRecord"] = "reference.refmanualindex.chapters.id-00001.subchapters.id-00001.refpages.id-00007@Storm Lord's Wrath",
+			},
+			["reference.refmanualindex@DD Princes of the Apocalypse"] = {
+				["sPageRecord"] = "reference.refmanualindex.chapters.chapter_0.subchapters.subchapter_06.refpages.refpage_07@DD Princes of the Apocalypse",
+			},
+			["reference.refmanualindex@DD Lost Mine of Phandelver"] = {
+				["sPageRecord"] = "reference.refmanualindex.chapters.chapter_0.subchapters.subchapter_3.refpages.refpage_00@DD Lost Mine of Phandelver",
+			},
+			["reference.refmanualindex@DD Tashas Cauldron of Everything"] = {
+				["sPageRecord"] = "reference.refmanualindex.chapters.id-00004.subchapters.id-00002.refpages.id-00003@DD Tashas Cauldron of Everything",
+			},
+			["reference.refmanualindex@DD Explorer's Guide to Wildemount"] = {
+				["sPageRecord"] = "reference.refmanualindex.chapters.chapter_0.subchapters.subchapter_08.refpages.refpage_03@DD Explorer's Guide to Wildemount",
+			},
+			["reference.refmanualindex@Fantasy Grounds Portals"] = {
+				["sPageRecord"] = "reference.refmanualindex.chapters.id-00001.subchapters.id-00002.refpages.id-00001@Fantasy Grounds Portals",
+			},
+			["reference.refmanualindex@D&D Legacy of the Crystal Shard - Campaign Guide"] = {
+				["sPageRecord"] = "reference.refmanualindex.chapters.chapter_00.subchapters.subchapter_01.refpages.refpage_02@D&D Legacy of the Crystal Shard - Campaign Guide",
+			},
+			["reference.refmanualindex@Sleeping Dragon's Wake"] = {
+				["sPageRecord"] = "reference.refmanualindex.chapters.id-00001.subchapters.id-00001.refpages.id-00001@Sleeping Dragon's Wake",
+			},
+			["reference.refmanualindex@Monster Loot - Volume 1 - Monster Manual"] = {
+				["sPageRecord"] = "reference.refmanualindex.chapters.id-00004.subchapters.id-00001.refpages.id-00001@Monster Loot - Volume 1 - Monster Manual",
+			},
+			["reference.refmanualindex@Divine Contention"] = {
+				["sPageRecord"] = "reference.refmanualindex.chapters.id-00001.subchapters.id-00001.refpages.id-00001@Divine Contention",
+			},
+			["reference.refmanualindex@WOTC50DMG"] = {
+				["sPageRecord"] = "reference.refmanualindex.chapters.id-00004.subchapters.id-00001.refpages.id-00016@WOTC50DMG",
+			},
+			["reference.refmanualindex@DD Tomb of Annihilation"] = {
+				["sPageRecord"] = "reference.refmanualindex.chapters.chapter_0.subchapters.subchapter_0.refpages.refpage_06@DD Tomb of Annihilation",
+			},
+			["reference.refmanualindex@DD Sword Coast Adventurer's Guide - Campaign Guide"] = {
+				["sPageRecord"] = "reference.refmanualindex.chapters.chapter_2.subchapters.subchapter_2.refpages.refpage_5@DD Sword Coast Adventurer's Guide - Campaign Guide",
+			},
+			["reference.refmanualindex@(X)TJ's Solo"] = {
+				["sPageRecord"] = "reference.refmanualindex.chapters.id-00001.subchapters.id-00001.refpages.id-00001@(X)TJ's Solo",
+			},
+			["reference.refmanualindex@5E Magic Shops & Random Item Rarity Tables"] = {
+				["sPageRecord"] = "reference.refmanualindex.chapters.id-00001.subchapters.id-00001.refpages.id-00005@5E Magic Shops & Random Item Rarity Tables",
+			},
+			["reference.refmanualindex@DD Guildmaster Guide to Ravnica - Krenko"] = {
+				["sPageRecord"] = "reference.refmanualindex.chapters.id-00001.subchapters.id-00004.refpages.id-00002@DD Guildmaster Guide to Ravnica - Krenko",
+			},
+			["reference.refmanualindex@DD Dragon of Icespire Peak"] = {
+				["sPageRecord"] = "reference.refmanualindex.chapters.chapter_0.subchapters.subchapter_10.refpages.refpage_00@DD Dragon of Icespire Peak",
+			},
+		},
+		["masterindex"] = {
+			["storytemplate"] = {
+				["category"] = "D&D Dungeon Master's Guide (Legacy)",
+			},
+			["skill"] = {
+				["category"] = "",
+			},
+			["tables"] = {
+				["category"] = "(Monster Loot)",
+			},
+			["encounter"] = {
+				["category"] = "(2. LDoP: The Pale Requiem )",
+			},
+			["class"] = {
+				["category"] = "(Homebrew)",
+			},
+			["npc"] = {
+				["category"] = "(TestingCodexHere)",
+			},
+			["notes"] = {
+				["category"] = "",
+			},
+			["soundset"] = {
+				["category"] = "((Q)WEAPON: Battleaxe)",
+			},
+			["battle"] = {
+				["category"] = "(DoIP Jade's Campaign)",
+			},
+			["item"] = {
+				["category"] = "(Key Items)",
+			},
+			["treasureparcels"] = {
+				["category"] = "",
+			},
+			["image"] = {
+				["category"] = "(Sword Coast)",
+			},
+			["vehicle"] = {
+				["category"] = "",
+			},
+			["shops"] = {
+				["category"] = "Westbridge",
+			},
+			["MKshops"] = {
+				["category"] = "",
+			},
+		},
+		["tokenbag"] = {
+			["history"] = "sPath|=|Data|&|sFilterType|=|token|||sPath|=|Data/Creature|&|sFilterType|=|token|||sFilterType|=|portrait|||sPath|=|Data|&|sFilterType|=|portrait|||sPath|=|Data|&|nDisplayIndex|=|33|&|sFilterType|=|portrait|||sPath|=|Data|&|sFilterSearch|=|luc|&|sFilterType|=|portrait|||sPath|=|Data|&|sFilterSearch|=|tobin|&|sFilterType|=|portrait|||sFilterType|=|token|||sPath|=|Data|&|sFilterType|=|token|||sPath|=|Data/Auras|&|sFilterType|=|token|||",
+			["viewzoom"] = 1,
+			["viewtype"] = "grid",
+			["lastfilter"] = "all",
+		},
+	},
+	["OptASGTGT"] = "autotarget",
+	["OptVALIDTGT"] = "anyfriendly",
+	["OptPSMN"] = "on",
+	["sidebarversion"] = 2,
+	["OptCTSH"] = "on",
+	["OptTNPCE"] = "on",
+	["OptCTSI"] = "off",
+	["OptTGMH"] = "bar",
+	["OptSOUND"] = "on",
+	["sidebar"] = "charsheet,note,image,table,story,quest,npc,battle,item,treasureparcel,background,class,feat,race,skill,spell",
+	["OptREVL"] = "on",
+	["OptINIT"] = "group",
+	["OptTBOX"] = "on",
+	["OptTNAM"] = "tooltip",
+	["OptGAVE"] = "2014",
+	["tabletopstate"] = {
+		[1] = {
+			["path"] = "tables",
+			["class"] = "masterindex",
+		},
+	},
+	["OptNNPC"] = "append",
+	["OptHRBASTION"] = "on",
+	["OptTPTY"] = "on",
+	["setup"] = true,
+	["OptTPCH"] = "bar",
+	["colortables"] = {
+		[""] = {
+			["color"] = "00FD2A",
+			["diceskin"] = 367,
+			["dicebodycolor"] = "000000",
+			["dicetextcolor"] = "34FF2C",
+		},
+	},
+	["OptSHRR"] = "on",
+	["OptCHATMAP"] = "token",
+	["OptFULLY_OWN_SHARED_CT_NPC"] = "on",
+	["OptCMAT"] = "on",
+}
